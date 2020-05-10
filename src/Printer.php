1f154a2251ed63c15a52a21bb66e5ab06a2d33d0
Jakub Bouček <pan@jakubboucek.cz>
2020-05-11 00:18:42 +0200

    Remove unnecessary PhpDoc

--- src/Printer.php
+++ src/Printer.php
@@ -7,9 +7,6 @@ class Printer
     /** @var IConnection */
     private $driver;
 
-    /**
-     * @param IConnection
-     */
     public function __construct(IConnection $driver)
     {
         $this->driver = $driver;
