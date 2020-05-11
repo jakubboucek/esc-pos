9f066111a89e00023f25bc27f89077528a939f5a
Jakub Bouček <pan@jakubboucek.cz>
2020-05-11 02:01:53 +0200

    Receipt is now fluent builder
    - Order Recep's tmethods

--- src/Receipt.php
+++ src/Receipt.php
@@ -14,24 +14,6 @@ class Receipt
         $this->init();
     }
 
-    public function __toString()
-    {
-        $this->finalize();
-        return implode("", $this->buffer);
-    }
-
-    public function test()
-    {
-        $output = "";
-        foreach ($this->buffer as $value) {
-            for ($i = 0, $len = strlen($value); $i < $len; $i++) {
-                $output .= "\\x" . sprintf('%02s', dechex(ord($value[$i])));
-            }
-            $output .= "\n";
-        }
-        return $output;
-    }
-
     private function init()
     {
         // ESC @
@@ -43,7 +25,7 @@ class Receipt
         $this->buffer[] = $data;
     }
 
-    public function finalize()
+    private function finalize()
     {
         $last = end($this->buffer);
         //                     LF      cut
@@ -56,6 +38,7 @@ class Receipt
     {
         // LF
         $this->buff("\n");
+        return $this;
     }
 
     public function feed($lines = 1)
@@ -65,6 +48,7 @@ class Receipt
             self::C_ESC . 'd'
             . chr($lines)
         );
+        return $this;
     }
 
     public function cut($fullCut = false)
@@ -74,6 +58,7 @@ class Receipt
             self::C_GS . "V"
             . chr($fullCut ? 0 : 1)
         );
+        return $this;
     }
 
     public function feedCut($feedBefore = 3, $fullCut = false)
@@ -84,11 +69,7 @@ class Receipt
             . ($fullCut ? "\x41" : "\x42")
             . chr($feedBefore)
         );
-    }
-
-    public function encodeOutput($data)
-    {
-        return iconv("UTF-8", "cp852//IGNORE", $data);
+        return $this;
     }
 
     public function write($data, $alreadyEncoded = false)
@@ -97,21 +78,14 @@ class Receipt
             $data = $this->encodeOutput($data);
         }
         $this->buff($data);
-    }
-
-    public function strlen($data, $encoded = false)
-    {
-        if ($encoded) {
-            return strlen($data);
-        }
-
-        return strlen($this->encodeOutput($data));
+        return $this;
     }
 
     public function writeLf($data, $encoded = false)
     {
         $this->write($data, $encoded);
         $this->lf();
+        return $this;
     }
 
     public function logo($kc1, $kc2, $sizeX = 1, $sizeY = 1)
@@ -127,13 +101,7 @@ class Receipt
             . chr($kc1) . chr($kc2)
             . chr($sizeX) . chr($sizeY)
         );
-    }
-
-    private function validateLogoKeyCode($keyCode)
-    {
-        if ($keyCode < 32 || $keyCode > 126) {
-            throw new InvalidKeyCodeException("Graphic key code expected between 32 and 126, $keyCode given.");
-        }
+        return $this;
     }
 
     public function ean13($data, $width = 3, $height = 42, $text = 0, $font = 0)
@@ -145,6 +113,7 @@ class Receipt
             . self::C_GS . "f" . chr($font)
             . self::C_GS . "k" . "\x02" . $data . "\x00"
         );
+        return $this;
     }
 
     public function code128($data, $width = 3, $height = 42, $text = 0, $font = 0)
@@ -157,46 +126,94 @@ class Receipt
             . self::C_GS . "f" . chr($font)
             . self::C_GS . "\x6bI" . chr(strlen($barData)) . $barData
         );
+        return $this;
     }
 
     public function left()
     {
         $this->buff(self::C_ESC . "\x61\x00");
+        return $this;
     }
 
     public function center()
     {
         $this->buff(self::C_ESC . "\x61\x01");
+        return $this;
     }
 
     public function right()
     {
         $this->buff(self::C_ESC . "\x61\x02");
+        return $this;
     }
 
     public function bold()
     {
         $this->buff(self::C_ESC . "\x45\x01");
+        return $this;
     }
 
     public function unbold()
     {
         $this->buff(self::C_ESC . "\x45\x00");
+        return $this;
     }
 
     public function fontA()
     {
         $this->buff(self::C_ESC . "\x4d\x00");
+        return $this;
     }
 
     public function fontB()
     {
         $this->buff(self::C_ESC . "\x4d\x01");
+        return $this;
     }
 
     public function fontSet($doubleW = 0, $doubleH = 0, $fontB = 0)
     {
         $code = ($doubleW ? 32 : 0) + ($doubleH ? 16 : 0) + ($fontB ? 1 : 0);
         $this->buff(self::C_ESC . "\x21" . chr($code));
+        return $this;
+    }
+
+    public function strlen($data, $encoded = false)
+    {
+        if ($encoded) {
+            return strlen($data);
+        }
+
+        return strlen($this->encodeOutput($data));
+    }
+
+    private function encodeOutput($data)
+    {
+        return iconv("UTF-8", "cp852//IGNORE", $data);
+    }
+
+    private function validateLogoKeyCode($keyCode)
+    {
+        if ($keyCode < 32 || $keyCode > 126) {
+            throw new InvalidKeyCodeException("Graphic key code expected between 32 and 126, $keyCode given.");
+        }
+    }
+
+    public function test()
+    {
+        $output = "";
+        foreach ($this->buffer as $value) {
+            for ($i = 0, $len = strlen($value); $i < $len; $i++) {
+                $output .= "\\x" . sprintf('%02s', dechex(ord($value[$i])));
+            }
+            $output .= "\n";
+        }
+        return $output;
+    }
+
+    public function __toString()
+    {
+        $this->finalize();
+        return implode("", $this->buffer);
     }
 }
